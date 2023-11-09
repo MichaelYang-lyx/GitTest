ad1c47114c56e8be1477dcc33ee1152263475a87
MichaelYang-lyx <yang.5915@buckeyemail.osu.edu>
2023-11-09 11:58:13 +0800

    1

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hi World fuck!';
+    return 'Hello World2000000!';
   }
 }
